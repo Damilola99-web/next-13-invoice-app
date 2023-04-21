07b93bb9b92bc14d273b64d5a708ab5b760e36b0
Damilola99-web <rasheedw622@gmail.com>
2023-04-21 20:04:56 +0100

    first production bug resolved I think.Wink face emoji

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -4,7 +4,8 @@ import useOutsideAlerter from './OutsideAlerter';
 export default function Filter({setFilterOpen}:{setFilterOpen : Dispatch<SetStateAction<boolean>>}) {
 	const filterRef = useRef<HTMLDivElement>();
 	useOutsideAlerter(filterRef, setFilterOpen);
-	return (
+    return (
+        // @ts-ignore
 		<div ref={filterRef} className='absolute bg-white dark:bg-dark-purple shadow-lg p-8 rounded-lg w-[220px] h-[150px] top-14 -left-[50%]'>
 			hey
 		</div>
