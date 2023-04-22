5dbe682eff76052346f5679c1eba92ea865d34f4
Damilola99-web <rasheedw622@gmail.com>
2023-04-22 23:44:10 +0100

    done qwith the filter component

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,22 +1,51 @@
-import React, { Dispatch, SetStateAction, useRef } from 'react';
+import { Check } from 'lucide-react';
+import React, { Dispatch, SetStateAction, useRef, useState } from 'react';
 import useOutsideAlerter from './OutsideAlerter';
 
-export default function Filter({setFilterOpen}:{setFilterOpen : Dispatch<SetStateAction<boolean>>}) {
+export default function Filter({
+	setFilterOpen,
+}: {
+	setFilterOpen: Dispatch<SetStateAction<boolean>>;
+}) {
 	const filterRef = useRef<HTMLDivElement>();
 	useOutsideAlerter(filterRef, setFilterOpen);
-    return (
-        // @ts-ignore
-		<div ref={filterRef} className='absolute bg-white dark:bg-dark-purple shadow-lg p-8 rounded-lg w-[220px] top-12 leftt flex flex-col space-y-2'>
+	const [filter, setFilter] = useState<'paid' | 'pending' | 'draft' | null>(null);
+	return (
+		<div
+			// @ts-ignore
+			ref={filterRef}
+			className='absolute bg-white dark:bg-dark-purple shadow-lg p-8 rounded-lg w-[220px] top-12 leftt flex flex-col space-y-2'
+		>
 			<div className='flex space-x-4 items-center'>
-				<span className=' flex w-4 h-4 rounded hover:border-deep-purple border-[1px] border-grey bg-grey'></span>
+				<span
+					style={filter === 'paid' ? { backgroundColor: '#7d5cfa' } : {}}
+					onClick={() => setFilter('paid')}
+					className=' items-center justify-center p-[2px] flex w-4 h-4 rounded hover:border-deep-purple border-[1px] border-grey bg-grey'
+				>
+					{filter === 'paid' && <Check className='text-white font-bold' />}
+				</span>
 				<p className='font-semibold text-sm'>Paid</p>
 			</div>
+
 			<div className='flex space-x-4 items-center'>
-				<span className=' flex w-4 h-4 rounded hover:border-deep-purple border-[1px] border-grey bg-grey'></span>
+				<span
+					style={filter === 'pending' ? { backgroundColor: '#7d5cfa' } : {}}
+					onClick={() => setFilter('pending')}
+					className=' items-center justify-center p-[2px] flex w-4 h-4 rounded hover:border-deep-purple border-[1px] border-grey bg-grey'
+				>
+					{filter === 'pending' && <Check className='text-white font-bold' />}
+				</span>
 				<p className='font-semibold text-sm'>Pending</p>
 			</div>
+
 			<div className='flex space-x-4 items-center'>
-				<span className=' flex w-4 h-4 rounded hover:border-deep-purple border-[1px] border-grey bg-grey'></span>
+				<span
+					style={filter === 'draft' ? { backgroundColor: '#7d5cfa' } : {}}
+					onClick={() => setFilter('draft')}
+					className=' items-center justify-center p-[2px] flex w-4 h-4 rounded hover:border-deep-purple border-[1px] border-grey bg-grey'
+				>
+					{filter === 'draft' && <Check className='text-white font-bold' />}
+				</span>
 				<p className='font-semibold text-sm'>Draft</p>
 			</div>
 		</div>
